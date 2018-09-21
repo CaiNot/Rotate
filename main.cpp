b976adf1407692225297841cde6e80a5c07a4cb5
CaiNot <303487454@qq.com>
2018-09-21 19:52:41 +0800

    添加了一个O(n)算法

--- main.cpp
+++ main.cpp
@@ -21,7 +21,22 @@ public:
     }
 
     void betterRotate(vector<int> &nums, int k) {
-        
+        int n = nums.size();
+        if (nums.empty() || k <= 0 || (k %= n) == 0)
+            return;
+
+        int i = 0;
+        int begin = 0;
+        int temp = nums[0];
+        for (int m = 0; m < n; ++m) {
+            i = (i + k) % n;// 变相地按照每隔k个数的规则来遍历数组
+            swap(temp, nums[i]); // temp保存了被替换的值
+
+            if (i == begin) {
+                temp = nums[++i];
+                begin = i;
+            }
+        }
     }
 
     void show(vector<int> &nums) {
@@ -35,9 +50,9 @@ public:
 int main() {
     std::cout << "Hello, World!" << std::endl;
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
-    int k = 2;
+    int k = 3;
     Solution s;
-    s.rotate(nums, k);
+    s.betterRotate(nums, k);
     s.show(nums);
     return 0;
 }
